c39d7842d502e2d76d69ab56f203d40ca6d5f7c6
Kéren Paula <keren.paula@etec.sp.gov.br>
2026-03-19 22:10:22 -0300

    Telas iniciais

---
+++ src/screens/ContasReceberScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import ContarPagarScreen from './ContasReceberScreen';
+
+export default function ContasPagarScreen(){
+    return(
+        <View>
+            <Text>Contas</Text>
+        </View>
+    );
+}
